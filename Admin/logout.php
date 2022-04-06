76d73d2b490f909442e92b081e3079dd75f9e7b2
TJH <90165815+1201200039@users.noreply.github.com>
2022-04-06 11:20:02 +0800

    Add files via upload

--- Admin/logout.php
+++ Admin/logout.php
@@ -1,17 +1,10 @@
 <?php
-include("Connection.php");
+include(dataconnection.php);
 session_start();
 
 unset($_SESSION['id']);
 
 session_destroy();
-?>
-		<script type="text/javascript">
-		alert("Logout Successfully!");
-		
-		</script>
-		
-	<?php 
-header("location:login.php");
 
+header("location:main.php");
 ?>
